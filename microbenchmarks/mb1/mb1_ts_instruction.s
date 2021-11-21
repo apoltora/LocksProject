1af9f6ca35ed9caced0c1c2eefa280493f10a6dc
Kiran Kumar Rajan Babu <krajanba@ghc67.ghc.andrew.cmu.edu>
2021-11-21 14:04:37 -0500

    fixed test and test and set with backoff lock

--- microbenchmarks/mb1/mb1_ts_instruction.s
+++ microbenchmarks/mb1/mb1_ts_instruction.s
@@ -5,10 +5,13 @@
 .global test_and_set
 .type test_and_set, %function
 
-# test_and_set:
-#     ts R0, mem[addr]
 
 test_and_set:
-    bts         %rax, %rdi
-    retq
+    mov $0, %rax # initialize rax to 0
+    # this instruction happens atomically
+    # copies the value in bit 0 of the memory location passed via %rdi to the Carry Flag
+    # sets the bit 0 value to 1 for the memory location passed via %rdi
+    lock bts $0, (%rdi) 
+    adc $0, %rax # now rax contains the CF...means the return value is CF
+    ret
 
