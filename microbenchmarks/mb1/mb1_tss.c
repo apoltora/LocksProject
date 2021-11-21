12c36038de260c97124541f4bc7eeb958cc2072e
Kiran Kumar Rajan Babu <krajanba@ghc67.ghc.andrew.cmu.edu>
2021-11-21 14:11:49 -0500

    fixed test and test and set lock

--- microbenchmarks/mb1/mb1_tss.c
+++ microbenchmarks/mb1/mb1_tss.c
@@ -1,6 +1,6 @@
 /**
  * This microbenchmark is a very simple add operation to a shared variable
- * Using test-and-test-and-set lock
+ * Using test-and-test-and-set with backoff lock
  * 
  * Use this command to compile:
  * gcc -o tss -lpthread mb1_tss.c mb1_ts_instruction.s
@@ -20,29 +20,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NUM_THREADS 128
+#define NUM_THREADS 10
 
 volatile int lock;
 volatile int x;
 
-extern volatile int test_and_set(volatile int L);
+extern int test_and_set(volatile int* L);
 
-void Lock(volatile int* l) {
+typedef struct {
+    int threadId;
+} WorkerArgs;
+
+void Lock(volatile int* l, int threadId) {
+    int waiting_time = 0;
     while (1) {
-        while (*l != 0);
-        if (test_and_set(*l) == 0) // need to complete writing the test_and_set in assembly (it requires ARM)
+        
+        // test operation
+        while (*l != 0)
+        {
+            //printf("Thread %d waiting here\n", threadId);
+            waiting_time++;
+        }
+
+        // test and set operation
+        // if the test and set returns 0...this means CF was 0 and the thread did acquire the lock.
+        if (test_and_set(l) == 0)
+        {
+            printf("Thread %d finished and returning here....waitied for %d cycles to acquire the lock\n", threadId, waiting_time);
             return;
+        }
+       
     }
 }
+
 void Unlock(volatile int* l) { 
     *l = 0;
 }
 
-void *operation(void *vargp) {
+void *operation(void *threadArgs) {
+
+    WorkerArgs* args = (WorkerArgs*) threadArgs;
+
+    //printf("Thread %d reached here\n", args->threadId);
+
     // place a start timer here
-    Lock(&lock);
+    Lock(&lock, args->threadId);
     // place an end timer here
     x++;
+    /* delay added here to spend some time in critical section so that we observe effects of backoff */
+    int delay = 100000;
+    while(delay)
+        delay--;
+
     Unlock(&lock);
     // place an end timer here
 }
@@ -51,14 +80,22 @@ void *operation(void *vargp) {
 int main() {
     x = 0;
     pthread_t threads[NUM_THREADS];
+    WorkerArgs args[NUM_THREADS];
     int i, j;
     lock = 0;
+
+    for (i=0; i<NUM_THREADS; i++) {
+        // Thread arguments...
+        args[i].threadId = i;
+    }
+
     for (i = 0; i < NUM_THREADS; i++) {
-        pthread_create(&threads[i], NULL, operation, NULL);    // make the threads run the operation function
+        pthread_create(&threads[i], NULL, operation, &args[i]);    // make the threads run the operation function
     }
     for (j = 0; j < NUM_THREADS; j++) {
         pthread_join(threads[j], NULL);                      // waits for all threads to be finished before function returns
     }
-    return x;
+    printf("The value of x is : %d\n", x);
+    return 0;
 }
 
