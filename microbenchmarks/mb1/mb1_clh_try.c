ea1019c1b4a06726bc9a37e61a55fd5531a320ed
Kiran Kumar Rajan Babu <krajanba@ghc67.ghc.andrew.cmu.edu>
2021-12-05 13:37:05 -0500

    get_time function pending

--- microbenchmarks/mb1/mb1_clh_try.c
+++ microbenchmarks/mb1/mb1_clh_try.c
@@ -26,6 +26,8 @@
 #define NUM_THREADS 8
 #define CACHE_LINE_SIZE 64 
 
+// Timeout threshold // TODO: tune this value
+#define T 0.0005 // 500 microsecs  
 
 
 typedef enum {waiting,      // lock is held
@@ -33,100 +35,249 @@ typedef enum {waiting,      // lock is held
             leaving,      // node owner is giving up
             transient,    // successor is giving up
             recycled}     // no pointers to node remain
-    clh_status;
+    qlock_state;
 
 
-typedef struct clh_qnode {
-    volatile clh_status status;
-    char padding[CACHE_LINE_SIZE - sizeof(clh_status)]; //padding
-    volatile struct clh_qnode *volatile prev;
-} clh_qnode;
+typedef struct qlock {
+    volatile qlock_state _Atomic status;
+    char padding[CACHE_LINE_SIZE - sizeof(qlock_state)]; //padding
+    struct qlock* volatile prev;
+} qlock_t;
 
 
-typedef volatile clh_qnode *clh_qnode_ptr;
-typedef clh_qnode_ptr clh_try_lock;
+// typedef volatile clh_qnode *clh_qnode_ptr;
+// typedef clh_qnode_ptr clh_try_lock;
 
-bool clh_try_acquire(clh_try_lock *L, clh_qnode_ptr I, hrtime_t T)
+qlock_t* volatile _Atomic glock;
+
+int x;
+
+
+// TODO: complete this
+double get_time_func()
 {
-    hrtime_t start;
-    clh_status stat;
 
-    I->status = waiting;
-    clh_qnode_ptr pred = swap(L, I);
 
-    // check lock once before querying timer -- optimization
+}
+
+
+bool AcquireQLock(qlock_t *mlock) 
+{
+    double start_time;
+    qlock_state stat;
+
+    mlock->status = waiting;
+    mlock->prev = NULL;
+
+    // predecessor in the queue
+    qlock_t *pred; 
+
+    while(1)
+    {
+        pred = glock;
+        // parameters are destination, expected value, desired value
+        if(atomic_compare_exchange_weak(&glock, &pred, mlock))
+            break;
+    }
+
+    // As an optimization, check the lock once before querying timer
     if (pred->status == available) {
-        I->prev = pred;
+        //TODO: uncomment the below code later to reclaim space
+        /*mlock->prev = NULL;
+        free(pred);*/
+        mlock->prev = pred;
+        // lock acquired
         return true;
     }
-    start = gethrtime();       // high resolution clock
+    
+    start_time = get_time_func();       
 
-    while (gethrtime() - start < T) {
+    // execute this while loop until the node times out
+    while (get_time_func() - start_time < T) {
         stat = pred->status;
         if (stat == available) {
-            I->prev = pred;
+            //TODO: uncomment the below code later to reclaim space
+            /*mlock->prev = NULL;
+            free(pred);*/
+            mlock->prev = pred;
+            // lock acquired
             return true;
         }
         if (stat == leaving) {
-            clh_qnode_ptr temp = pred->prev;
+            qlock_t *temp = pred->prev;
             pred->status = recycled;
             pred = temp;
         }
-        // stat might also be transient,
-        // if somebody left the queue just before I entered
+        // stat can also be transient,
+        // if some node has left the queue just before my node entered
     }
 
-    // timed out
+
+    // my node timed out at this point
 
     while (1) {
-        while (pred->status == transient);  // spin
-        stat = swap(&pred->status, transient);
+
+        /* there is possiblility that pred's status can be transient while entering this while loop as some other node might be in the process 
+        of leaving the queue. 
+        if so just spin until state changes to waiting again */
+        while (pred->status == transient); 
+
+        while(1)
+        {
+            stat = pred->status;
+            // parameters are destination, expected value, desired value
+            if(atomic_compare_exchange_weak(&pred->status, &stat, transient))
+                break;
+        }
+
+
         if (stat == available) {
-            I->prev = pred;
+            //TODO: uncomment the below code later to reclaim space
+            /*mlock->prev = NULL;
+            free(pred);*/
+            mlock->prev = pred;
+            // lock acquired
             return true;
         }
 
+        // if pred is leaving help pred to leave...
         if (stat == leaving) {
-            clh_qnode_ptr temp = pred->prev;
+            qlock_t *temp = pred->prev;
             pred->status = recycled;
             pred = temp;
             continue;
         }
-        break;          // stat == waiting
+        break; // stat == waiting
     }
-    I->prev = pred;     // so successor can find it
+
+    /* updating prev pointer (as pred might have changed by now) so that successor can find it */
+    mlock->prev = pred; 
 
     // indicate leaving, so successor can't leave
     while (1) {
-        stat = compare_and_swap(&I->status, waiting, leaving);
-        if (stat == waiting) break; //swap success
+        stat = waiting;
+        atomic_compare_exchange_weak(&mlock->status, &stat, leaving);
+
+        //swap success
+        if (stat == waiting) 
+            break; 
+
         // spin until the status becomes waiting again
-        while (I->status != waiting);
+        while (mlock->status != waiting);
     }
 
+
+    /******* START LEAVING FROM THE QUEUE *********/
+
     /* special case: if last thread in the queue, swap the lock tail with pred and leave */
-    if (compare_and_store(L, I, pred)) {
+
+    qlock_t *mlock_temp = mlock;
+
+    if(atomic_compare_exchange_weak(&glock, &mlock_temp, pred)){
         pred->status = waiting;
+
+        /* no need to free mlock here as we are anyways going to do retry with mlock after sometime */
         return false;
     }
 
-    // not last in the queue
+    // node is not the last in the queue
     while (1) {
-        stat = I->status;
+        stat = mlock->status;
         if (stat == recycled) {
             pred->status = waiting;
+
+            /* no need to free mlock here as we are anyways going to do retry with mlock after sometime */
             return false;
         }
     }
 }
 
-void clh_release(clh_qnode_ptr *I)
+
+
+void ReleaseQLock(qlock_t *mlock)
 {
-    clh_qnode_ptr pred = (*I)->prev;
-    while (!compare_and_store(&(*I)->status, waiting, available)) {
-        // can't set my node to available if it's currently transient
-        while ((*I)->status == transient);  // spin
+    qlock_state temp_state = waiting;
+    // If status is waiting change it to available (atomically)
+ while (!atomic_compare_exchange_weak(&mlock->status, &temp_state, available)) {
+        /* can't set my node to available if it's currently transient, so start spinning */
+        while (mlock->status == transient);  
+        temp_state = waiting;
     }
-    *I = pred;
+}
+
+
+
+void *operation(void *vargp) {
+    // place a start timer here
+    qlock_t *mylock;
+    mylock = (qlock_t *) malloc(sizeof(qlock_t));
+
+    bool acquire_status = true;
+   
+    while(1)
+    {
+        acquire_status = AcquireQLock(mylock);
+
+        if(acquire_status) // lock acquire success
+            break;
+        else
+        {
+            // as the lock timed out, yield now and continue later
+            sched_yield();
+
+          //? maybe we should wait for sometime here before a retry ?
+        }
+
+
+    }
+
+    /**** CRITICAL SECTION *****/
+
+    // place an end timer here
+    x++;
+    long delay = 1000000000;
+    while(delay)
+        delay--;
+
+    ReleaseQLock(mylock);
+
+    // place an end timer here
+    return vargp;
+}
+
+
+
+int main() {
+    x = 0;
+
+    // initialize glock
+    glock = NULL;
+    qlock_t *glock_init;
+    glock_init = (qlock_t *) malloc(sizeof(qlock_t));
+
+    glock_init->status = available;
+    glock_init->prev = NULL;
+
+    qlock_t *prev_glock = glock;
+    atomic_compare_exchange_weak(&glock, &prev_glock, glock_init);
+
+    // Create and spawn threads
+    pthread_t threads[NUM_THREADS];
+    int i, j;
+
+    for (i = 0; i < NUM_THREADS; i++) {
+        pthread_create(&threads[i], NULL, operation, NULL);    // make the threads run the operation function
+    }
+
+    for (j = 0; j < NUM_THREADS; j++) {
+        pthread_join(threads[j], NULL);                      // waits for all threads to be finished before function returns
+    }
+
+    printf("The value of x is : %d\n", x);
+
+    // free the final tail node of glock
+    free(glock);
+    return 0;
+
 }
 
